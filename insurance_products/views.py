bcf888fb7a0e5d691db56c61923d684986ccd047
minhtb27112004 <nguyendacminh2711@gmail.com>
2025-09-21 00:13:53 +0700

    feature filter policy

--- insurance_products/views.py
+++ insurance_products/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
+from django.db import models
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from .models import InsuranceProduct
 def custom_products_admin(request):
     return render(request, 'admin/products_section.html')
+
+
+
