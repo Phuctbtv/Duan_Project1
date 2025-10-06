c64293d6bfae145a7cfb519c2fce1eb2c4f49a95
Phuctbtv <167066175+Phuctbtv@users.noreply.github.com>
2025-10-07 01:25:10 +0700

    add CRUD to contract and complete search functionality

--- policies/urls.py
+++ policies/urls.py
@@ -1,7 +1,37 @@
+# from django.urls import path
+# from . import views
+#
+# urlpatterns = [
+#     path('admin/policies/', views.custom_policies_admin, name='custom_policies_admin'),
+#     path('user/policies/dashboard', views.dashboard_view_user, name='custom_policies_users'),
+# ]
+#
+#
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ADMIN
     path('admin/policies/', views.custom_policies_admin, name='custom_policies_admin'),
-    path('user/policies/dashboard', views.dashboard_view_user, name='custom_policies_users'),
+
+    # Danh sách hợp đồng
+    path('admin/policies/list/', views.admin_policy_list, name='admin_policy_list'),
+
+    # Tạo hợp đồng mới
+    path('admin/policies/add/', views.admin_policy_create, name='policy_create'),
+
+    # Xem chi tiết hợp đồng
+    path('admin/policies/<int:pk>/', views.admin_policy_detail, name='policy_detail'),
+
+    # Gia hạn hợp đồng
+    path('admin/policies/<int:pk>/renew/', views.admin_policy_renew, name='policy_renew'),
+
+    # Hủy hợp đồng
+    path('admin/policies/<int:pk>/cancel/', views.admin_policy_cancel, name='policy_delete'),
+
+    # USER
+    path('user/policies/dashboard/', views.dashboard_view_user, name='custom_policies_users'),
+
 ]
+
+
